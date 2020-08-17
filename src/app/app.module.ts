b9dc2e3d76569130cecb27ef2a056e95a3926b75
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2020-08-17 13:44:04 +0700

    Added mat tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { JsPanelComponent } from './js-panel/js-panel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { JsPanelComponent } from './js-panel/js-panel.component';
     JsPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
